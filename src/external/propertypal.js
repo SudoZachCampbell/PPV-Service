ca7cfddfa5f271c42c2f8a11eed49d80e10f38fa
40132243 <zcampbell03@qub.ac.uk>
2019-10-28 13:45:09 +0000

    Multiple Page URL Gathering Supported

--- src/external/propertypal.js
+++ src/external/propertypal.js
@@ -1,15 +1,22 @@
 var request = require('request');
 
 module.exports = {
-    getPropertyByArea: (area) => {
+    getPropertySearchByArea: (area, page) => {
         return new Promise((resolve, reject) => {
-            request.get(`https://www.propertypal.com/property-to-rent/${area}`, (error, response, body) => {
-                resolve(body);
-            });
+            if (page === 1) {
+                request.get(`https://www.propertypal.com/property-to-rent/${area}`, (error, response, body) => {
+                    resolve(body);
+                });
+            } else {
+                request.get(`https://www.propertypal.com/property-to-rent/${area}/page-${page}`, (error, response, body) => {
+                    resolve(body);
+                });
+            }
         });
     },
     getPropertyByUrl: (url) => {
         return new Promise((resolve, reject) => {
+            console.log(`Getting Property: ${url}`);
             request.get(`https://www.propertypal.com${url}`, (error, response, body) => {
                 resolve(body);
             });
--- src/services/manageProperty.js
+++ src/services/manageProperty.js
@@ -1,3 +1,4 @@
+'use strict'
 var propertyPal = require('../external/propertypal');
 var _ = require('lodash');
 var jsdom = require('jsdom');
@@ -5,21 +6,33 @@ const { JSDOM } = jsdom;
 
 module.exports = {
     getProperty: async (area) => {
-        var body = await propertyPal.getPropertyByArea(area)
+        var body = await propertyPal.getPropertySearchByArea(area, 1)
         var body = new JSDOM(body);
         var window = body.window;
-        var propertyList = _.values(window.document.getElementById('body').querySelector('.sr .maxwidth .sr-body .sr-widecol .boxlist').getElementsByTagName('li'));
-        propertyList = propertyList.reduce((accum, prop) => {
-            if (!prop.classList.contains('boxlist-strech')) {
-                accum.push(prop.querySelectorAll('div a')[1].getAttribute('href'));
-            }
-            return accum;
-        }, []);
+        var page = window.document.querySelector('.paging-numbers .paging .paging-last a');
+        if (page) {
+            page = page.innerHTML
+        }
+        console.log(`Pages: ${page}`);
+        var propertyList = [];
+        for(var i = 0; i < page; i++) {
+            console.log(`Iterating Page Number: ${i+1}`);
+            body = await propertyPal.getPropertySearchByArea(area, i+1)
+            body = new JSDOM(body);
+            window = body.window;
+            let propertyListArray = _.values(window.document.getElementById('body').querySelector('.sr .maxwidth .sr-body .sr-widecol .boxlist').getElementsByTagName('li'));
+            propertyListArray.reduce((accum, prop) => {
+                if (!prop.classList.contains('boxlist-strech') && !prop.querySelector('div').classList.contains('propbox-premium-advert')) {
+                    accum.push(prop.querySelectorAll('div a')[1].getAttribute('href'));
+                }
+                return accum;
+            }, propertyList);
+        }
         var propertyPromiseArray = propertyList.reduce((accum, url) => {
             accum.push(propertyPal.getPropertyByUrl(url));
             return accum;
         }, []);
         var propertyReturns = await Promise.all(propertyPromiseArray);
-        return propertyReturns;
+        return propertyList;
     }
 }
